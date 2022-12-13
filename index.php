69955c685afa079b02fffc8fd1003b226413ee2d
andreadldev <andrea.delisi2@gmail.com>
2022-12-13 17:22:45 +0100

    Paragrafo, lunghezza paragrafo e input

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+$text = "PHP è un linguaggio di scripting general-purpose open source molto utilizzato, specialmente indicato per lo sviluppo web perchè può essere facilmente integrato nell'HTML. Nasce nel 1994 come progetto personale (originalmente l’acronimo era Personal Home Page), poi è
+stato adottato da una ampia community che lo ha migliorato (e trasformato) fino alla recentissima
+versione PHP 8, rilasciata appena il 26 novembre 2020.";
+$word = $_GET['word'] ?? null;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +13,16 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <h3>Paragrafo:</h3>
+    <span><?php echo $text?></span>
+
+    <h3>Lunghezza paragrafo:</h3>
+    <span><?php echo strlen($text)?> parole</span>
+
+    <h3>Paragrafo con parola censurata:</h3>
+    <form method="get">
+        <input type="text" name="word" placeholder="Parola da censurare">
+    </form>
+    <span><?php echo str_replace($word,"***", $text) ?></span>
 </body>
 </html>
